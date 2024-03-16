d8b6aadbd9864b3d57e3fbfd86afaf01baca188b
Jesus <delgadoperdomoje@uvm.edu.ve>
2024-03-16 16:42:31 -0400

    5to commit: Se empezó con la página listado

--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -1,8 +1,26 @@
+import {useEffect, useState } from "react";
 import estilos from "../css/Listado.module.css";
-import React from 'react'
+export function Listado({dark}) {
+  const [peliculas, setPeliculas]= useState([])
 
-export function Listado() {
+ const buscarPelicula = () => {
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=f076f66e04aa391b44e7022553ca7009&language=es-ES&page=1")
+      .then((res) => res.json())
+      .then((json) => setPeliculas(json.results));
+  };
+  useEffect(() => {
+    buscarPelicula();
+  }, []);
+
   return (
-    <div className={estilos.principal}>Listado</div>
-  )
+    <>
+    <ul className={dark ? estilos.claro : estilos.oscuro}>
+        {peliculas.map((pelicula) => (
+            <li>
+                <div className={estilos.listar} key={pelicula.id}> {pelicula.title}</div>
+            </li>
+        ))}
+    </ul>
+    </>
+  );
 }
