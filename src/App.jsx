924ca1233e3781c048bc59e7ebeb147a51d25901
Jesus <delgadoperdomoje@uvm.edu.ve>
2024-03-16 16:54:20 -0400

    7mo Commit: Terminada la página de listado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Listado } from "./componentes/Listado";
 import { Inicio } from "./componentes/Inicio";
+import { Tendencias } from "./componentes/Tendencias";
 import { Navbar } from "./componentes/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState } from "react";
-import Nosotros from "./componentes/Nosotros";
-import Tendencias from "./componentes/Tendencias";
+import { Nosotros } from "./componentes/Nosotros";
 
 export function App() {
   const [dark,setdark] = useState(true);
   const [pagina,setpaginasig] = useState(1);
+  const [buscarp,setBuscarp] = useState("");
   function darklight(dark){
     setdark(!dark);
   }
@@ -24,16 +25,21 @@ export function App() {
       setpaginasig(pagina)
     }
   }
-
+  //función de búsqueda
+  function buscar(buscarp) {
+    setBuscarp(buscarp)   
+  }
+  
   return (
     <BrowserRouter>
-      <Navbar  dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)}/>
+      <Navbar  dark={dark} cambiarmodo={(dark)=>darklight(dark)} />
       <Routes>
-        <Route path="/" element={<Inicio dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)} />} />
-        <Route path="/Listado" element={<Listado />}/>
-        <Route path="/Tendencias" element={<Tendencias />}/>
-        <Route path="/Nosotros" element={<Nosotros />}/>
+        <Route path="/" element={<Inicio dark={dark} cambiarmodo={(dark)=>darklight(dark)} />} />
+        <Route path="/Listado" element={<Listado dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)} buscarp={buscarp} buscarpeli={(buscarp)=>buscar(buscarp)}/>}/>
+        <Route path="/Tendencias" element={<Tendencias dark={dark} cambiarmodo={(dark)=>darklight(dark)} />} />
+        <Route path="/Nosotros" element={<Nosotros dark={dark} cambiarmodo={(dark)=>darklight(dark)} />}/>
       </Routes>
     </BrowserRouter>
   );
 }
+
--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -1,20 +1,38 @@
 import {useEffect, useState } from "react";
+import { useDebounce } from "../utilidades/useDebounce";
 import estilos from "../css/Listado.module.css";
 import { Miniatura } from "./Miniatura";
-export function Listado({dark}) {
+export function Listado({dark, pagina}) {
   const [peliculas, setPeliculas]= useState([])
+  const [buscarp,setBuscarp] = useState("");
+  const debounceValue = useDebounce(buscarp, 300);
 
   const buscarPelicula = () => {
-    fetch("https://api.themoviedb.org/3/discover/movie?api_key=f076f66e04aa391b44e7022553ca7009&language=es-ES&page=1")
+  if (buscarp.length>0){
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${buscarp}&include_adult=false&language=es-ES&api_key=f076f66e04aa391b44e7022553ca7009&page=${pagina}`)
       .then((res) => res.json())
       .then((json) => setPeliculas(json.results));
+  }else{
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=f076f66e04aa391b44e7022553ca7009&language=es-ES&page="+pagina)
+      .then((res) => res.json())
+      .then((json) => setPeliculas(json.results));
+  }
+
   };
   useEffect(() => {
     buscarPelicula();
-  }, []);
+  }, [debounceValue, pagina]);
+
+  const evaluarCambios = ({ target }) => {
+    setBuscarp(target.value);
+  };
 
   return (
     <>
+    <div className={estilos.contenedor}>
+            <img className={estilos.imgbuscar} src="./buscar.png" alt="" />
+            <input type="text" value={buscarp} onChange={evaluarCambios} className={estilos.txtbuscar} placeholder="Buscar" />
+    </div>
     <ul className={dark ? estilos.claro : estilos.oscuro}>
       {peliculas.map((pelicula) => (
         <Miniatura key={pelicula.id} dark={dark} pelicula={pelicula} />
