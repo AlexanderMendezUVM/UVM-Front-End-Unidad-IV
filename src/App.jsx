143235a159a9b43cb5ace07e283d48c5b44755fc
AaronRsls <rosalesramirezaa@uvm.edu.ve>
2024-03-16 21:23:15 -0400

    Commit 11: Añadido Debounce y ver trailers en youtube

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Listado } from "./componentes/Listado";
+import { Detalles } from "./componentes/Detalles";
 import { Inicio } from "./componentes/Inicio";
 import { Tendencias } from "./componentes/Tendencias";
 import { Navbar } from "./componentes/Navbar";
@@ -29,17 +30,16 @@ export function App() {
   function buscar(buscarp) {
     setBuscarp(buscarp)   
   }
-  
   return (
     <BrowserRouter>
-      <Navbar  dark={dark} cambiarmodo={(dark)=>darklight(dark)} />
+      <Navbar  dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)}/>
       <Routes>
-        <Route path="/" element={<Inicio dark={dark} cambiarmodo={(dark)=>darklight(dark)} />} />
+        <Route path="/" element={<Inicio dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)} />} />
         <Route path="/Listado" element={<Listado dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)} buscarp={buscarp} buscarpeli={(buscarp)=>buscar(buscarp)}/>}/>
-        <Route path="/Tendencias" element={<Tendencias dark={dark} cambiarmodo={(dark)=>darklight(dark)} />} />
+        <Route path="/Listado/:idPelicula" element={<Detalles dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)} />} />
+        <Route path="/Tendencias" element={<Tendencias dark={dark} cambiarmodo={(dark)=>darklight(dark)} pagina={pagina} cambiarpagina={(pagina)=>cambiopag(pagina)} />} />
         <Route path="/Nosotros" element={<Nosotros dark={dark} cambiarmodo={(dark)=>darklight(dark)} />}/>
       </Routes>
     </BrowserRouter>
   );
 }
-
