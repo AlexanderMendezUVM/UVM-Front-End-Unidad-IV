3e369ce7e700d4b8ce1e94f88f582edda5d2fb47
JoseAdrian1994 <barriosbricenojoad@uvm.edu.ve>
2024-03-17 14:36:41 -0400

    Commit 12: Empezó con las tendencias

--- src/componentes/Tendencias.jsx
+++ src/componentes/Tendencias.jsx
@@ -1,8 +1,44 @@
 import estilos from "../css/Tendencias.module.css";
-import React from 'react'
-
-export function Tendencias() {
+import { useEffect, useState } from "react";
+export function Tendencias({dark}) {
+  const rutaminiatura = "https://image.tmdb.org/t/p/w500";
+  const [movieList1, setMovieList1] = useState([]);
+  const getMovie = () => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=f076f66e04aa391b44e7022553ca7009&language=es-ES"
+    )
+      .then((res) => res.json())
+      .then((json) => setMovieList1(json.results));
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return (
-    <div className={estilos.principal}>Tendencias</div>
-  )
+    <>
+      <div className={dark ? estilos.slideroscuro : estilos.sliderclaro}>
+        <div className={estilos.slidetrack}>
+          {movieList1.map((pelicula1) => (
+            <div className={estilos.slide}>
+              <img
+                style={{ width: "350px", height: "480px" }}
+                className={estilos.imgSlider}
+                src={rutaminiatura + pelicula1.poster_path}
+                alt=""
+              />
+            </div>
+          ))}
+          {movieList1.map((pelicula1) => (
+            <div className={estilos.slide}>
+              <img
+                style={{ width: "350px", height: "480px" }}
+                className={estilos.imgSlider}
+                src={rutaminiatura + pelicula1.poster_path}
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
